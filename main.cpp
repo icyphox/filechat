7b03ac4a1dae518f3c4f32c240fbb31cdbb56df4
Icyphox <icyph0x@protonmail.com>
2016-10-07 22:50:39 +0530

    Create main.cpp (add lol stuff in it)

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Chat{
+    /* This is the shyts that gets written
+     * into the text file.
+     * P.S.: Vim rocks.
+     */
+};
+
+
+int main(){
+    /*This is where the stuff comes*/
+}
+
+
