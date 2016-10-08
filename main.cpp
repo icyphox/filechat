476c6ec9a8f8fa2dc79322fab4ca773f2b91536c
Icyphox <icyph0x@protonmail.com>
2016-10-08 16:42:40 +0530

    Made some meaningful additions Niggler friendly braces too

--- main.cpp
+++ main.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <cstdio>
 
 using namespace std;
 
-class Chat{
-    /* This is the shyts that gets written
-     * into the text file.
-     * P.S.: Vim rocks.
-     */
+struct Chat
+{
+    char username[20];
+    char message[50]; //note to self: make this a pointer
 };
 
+void msgToFile(Chat sendmsg)
+{
+    // writing message to file
+    ofstream tofile;
+    tofile.open("chat.dat", ios::binary | ios::out);
+    tofile.write((char*)&sendmsg.username, sizeof(sendmsg.username));
+    tofile.write((char*)&sendmsg.message, sizeof(sendmsg.message));
+    tofile.close();
+}
 
-int main(){
-    /*This is where the stuff comes*/
+void getMsg()
+{
+    Chat getMessage;
+
+    ifstream fromfile;
+    fromfile.open("chat.dat", ios::binary | ios::in);
+    // honestly, I don't know how to do this reading crap
+    fromfile.close();
+}
+
+int main()
+{
+    Chat userEntry;
+    cout << "\t\t\t\tFileChat\n\n";
+    cout << "\t\tGrade 12 Semester 2 Project\n\n\n";
+    cout << "Enter the username you wish to use: ";
+    gets(userEntry.username);
+    // need to do some sort of loop her;
+    cout << "Message: "
+    gets(userEntry.message);
+
+    msgToFile(userEntry);
+    return 0;
 }
 
 
