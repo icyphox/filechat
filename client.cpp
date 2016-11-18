5ca0f28dbbaed2395a25dbdc3426d950af522f21
Anirudh Oppiliappan <icyph0x@protonmail.com>
2016-11-18 17:44:40 +0530

    Got this to work on Windows \o/
    Ending each message with # looks kinda halli, but I don't know of any workaround. If you can think of any, it'd be great. Now, to write the recieved message into a file, and display the message from that file.

--- client.cpp
+++ client.cpp
@@ -1,105 +1,72 @@
-
 #include <iostream>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-<<<<<<< HEAD
-#include <cstdlib>
-#define BUFSIZE 1024
-=======
+#include <winsock2.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
 
-
-
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
 using namespace std;
 int main()
 {
     int client;
-    int port = 1500; 
+    int port = 1500;
     int BUFSIZE = 1024;
     bool isExit = false;
     char buffer[BUFSIZE];
-<<<<<<< HEAD
     char* ip = "127.0.0.1"; /* fill this in later */
     struct sockaddr_in serverAddr; /* something for the server address AFAIK :P */
     // the socket gets created here
-    client = socket(AF_INET, SOCK_STREAM, 0); /* AF_INET is ipv4 protocol,
-                                             and SOCK_STREAM is for sending TCP packets */
-    if(client < 0) // since socket() will return -1 if the call fails
-=======
-    char* ip = "127.0.0.1";
+    WSADATA wsa;
+    //char username[20];
 
-    struct sockaddr_in server_addr;
-
-    client = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (client < 0) 
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
+    cout << "[*] Initialising Winsock." << endl;
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
     {
-        cout << "[!] Error creating socket." << endl;
-        exit(1);
+        cout << "[!] Failed. Error Code:" << WSAGetLastError();
+        return 1;
+    } /* AF_INET is ipv4 protocol,
+                                             and SOCK_STREAM is for sending TCP packets */
+     // since socket() will return -1 if the call fails
+
+
+    cout << "[:)] Initialised!" << endl;
+
+
+
+    if((client = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+    {
+        cout << "[!] Could not create socket: " << WSAGetLastError();
     }
-<<<<<<< HEAD
+
     else
         cout<< "[*] Socket client has been created successfully." << endl;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr=inet_addr(ip);
     serverAddr.sin_port = htons(port); // htons() converts port number to network byte order from host byte order
     // connecting to the socket happens here
-    if(connect(client, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == 0)
+    if(connect(client, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
     {
-        cout << "[*] Connection to the server port number " << port << endl;
+        cout << "[!] Failed to connect to " << port << endl;
+        return 1;
     }
     // the receiving happens here
-    cout << "[*] Waiting for confirmation from server" << endl;
+    cout << "[*] Waiting for confirmation from server." << endl;
     recv(client, buffer, BUFSIZE, 0);
     cout << "[:)] Connection confirmed!" << endl;
-    cout << "[*] Enter your username" << endl;
-    cout << "Username: " << endl;
-    gets(username);
-    cout <<"[!] Enter ~ (i.e., Shift + 2), to end this connection" << endl;
+   // cout << "[*] Enter your username" << endl;
+   // cout << "Username: " << endl;
+    //gets(username);
+    cout <<"[!] Enter # (i.e., Shift + 3), to end this connection" << endl;
     // client's message goes first
-    do{
-        cout << username << ": ";
-        do{
-=======
-
-    cout << "[*] Socket client has been created." << endl;
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-
-    if (connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
-        cout << "[*] Connection to the server port number: " << port << endl;
-
-    cout << "[*] Awaiting confirmation from the server." << endl; 
-    recv(client, buffer, BUFSIZE, 0);
-    cout << "[:)] Connection confirmed!" << endl;;
-
-    cout << "[*] Enter ~ to end this connection." << endl;
-
-
-    do {
-        cout << "Client: "; // We'll figure out the sending of client and server username later.
+   do {
+        cout << "Client: ";
         do {
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
             cin >> buffer;
             send(client, buffer, BUFSIZE, 0);
             if (*buffer == '#') {
                 send(client, buffer, BUFSIZE, 0);
-                *buffer = '*'; // Have to end each message with this character. Can't seem to figure out a workaround.
+                *buffer = '*';
                 isExit = true;
             }
-<<<<<<< HEAD
-        }while(*buffer != 0);
-    }while(isExit == 0);
-    cout << "[*] Connection terminated." << endl;
-=======
         } while (*buffer != 42);
 
         cout << "Server: ";
@@ -107,7 +74,7 @@ int main()
             recv(client, buffer, BUFSIZE, 0);
             cout << buffer << " ";
             if (*buffer == '#') {
-                *buffer = '*'; // Same here.
+                *buffer = '*';
                 isExit = true;
             }
 
@@ -116,10 +83,8 @@ int main()
 
     } while (!isExit);
 
-
     cout << "[!] Connection terminated with server.";
 
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
-    close(client);
+    closesocket(client);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,105 +1,63 @@
 #include <iostream>
 #include <string.h>
-<<<<<<< HEAD
-#include <stdlib.h>
-#include <winsock2.h>
 #include <stdio.h>
-=======
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <unistd.h>
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
+#include <winsock2.h>
 
 
 using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     SOCKET client, server;
-    WSADATA wsa;
-=======
-    int client, server;
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
     int port = 1500;
     int BUFSIZE =  1024;
     bool isExit = false;
     char buffer[BUFSIZE];
-<<<<<<< HEAD
 
     struct sockaddr_in server_addr;
+    int sze;
 
-    cout<<"[*] Initialising Winsock..."<<endl;
+    WSADATA wsa;
+
+    cout << "[*] Initialising Winsock." << endl;
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
     {
-        cout<<"[!] Failed. Error Code : "<<WSAGetLastError();
+        cout << "[!] Failed. Error Code:" << WSAGetLastError();
         return 1;
     }
 
-    cout<<"[*] Initialized."<<endl;
+    cout << "[:)] Initialised!" << endl;
+
+
 
     if((client = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
     {
-        cout<<"Could not create socket : "<<WSAGetLastError();
+        cout << "[!] Could not create socket: " << WSAGetLastError();
     }
 
-    cout<<"Socket created.\n";
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_addr.sin_port = htons(port);
-
-
-    if ((connect(client, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0)
-=======
-
-    struct sockaddr_in server_addr;
-    socklen_t size;
-
-    client = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (client < 0)
-    {
-        cout << "[!]Error establishing socket." << endl;
-        exit(1);
-    }
-
-    
-
-    cout << "[*] Socket server has been created." << endl;
+    cout << "[*] Socket server has been created.\n" << endl;
 
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    server_addr.sin_addr.s_addr = htons(INADDR_ANY); // any address can attempt to connect
     server_addr.sin_port = htons(port);
 
 
-    if ((bind(client, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0)
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
+    if ((bind(client, (struct sockaddr*)&server_addr, sizeof(server_addr))) < 0)
     {
         cout << "[!] Error binding connection, the socket has already been established..." << endl;
         return -1;
     }
 
 
-<<<<<<< HEAD
-=======
-    size = sizeof(server_addr);
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
+    sze = sizeof(server_addr);
     cout << "[*] Looking for clients." << endl;
 
     listen(client, 1);
 
 
     int clientCount = 1;
-<<<<<<< HEAD
-    server = accept(client,(struct sockaddr *)&server_addr, sizeof(&server_addr));
-=======
-    server = accept(client,(struct sockaddr *)&server_addr,&size);
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
+    server = accept(client,(struct sockaddr *)&server_addr,&sze);
 
     // first check if it is valid or not
     if (server < 0)
@@ -110,13 +68,9 @@ int main()
         strcpy(buffer, "[:)] Server connected!");
         send(server, buffer, BUFSIZE, 0);
         cout << "[*] Connected with the client. " << clientCount << endl;
-        cout << "[*] Enter ~ to end the connection." << endl;
+        cout << "[*] Enter # (i.e. Shift + 3), to end the connection." << endl;
 
-<<<<<<< HEAD
 
-=======
-      
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
 
         cout << "Client: ";
         do {
@@ -145,18 +99,14 @@ int main()
                 recv(server, buffer, BUFSIZE, 0);
                 cout << buffer << " ";
                 if (*buffer == '#') {
-                    *buffer == '8';
+                    *buffer == '*';
                     isExit = true;
                 }
             } while (*buffer != '*');
         } while (!isExit);
 
         cout << "[!] Connection terminated with IP " << inet_ntoa(server_addr.sin_addr);
-<<<<<<< HEAD
         closesocket(server);
-=======
-        close(server);
->>>>>>> ca6cc969428af0360ddce0fa63820b10a715e9c7
         isExit = false;
         exit(1);
     }
