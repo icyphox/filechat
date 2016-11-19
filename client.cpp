f9e0a26c54db15804874153c4e6c491573e7ae72
unknown <nikhilpe@gmail.com>
2016-11-19 07:10:02 +0530

    client: Add initial commit for username and option for dynamic destination IP

--- client.cpp
+++ client.cpp
@@ -9,14 +9,14 @@ int main()
 {
     int client;
     int port = 1500;
+    char username[120];
     int BUFSIZE = 1024;
     bool isExit = false;
     char buffer[BUFSIZE];
-    char* ip = "127.0.0.1"; /* fill this in later */
+    char ip[12];
     struct sockaddr_in serverAddr; /* something for the server address AFAIK :P */
     // the socket gets created here
     WSADATA wsa;
-    //char username[20];
 
     cout << "[*] Initialising Winsock." << endl;
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
@@ -39,6 +39,9 @@ int main()
 
     else
         cout<< "[*] Socket client has been created successfully." << endl;
+    //accept IP address of the target
+    cout<<"[*] Enter the IP address of the recipient: "<<endl;
+    gets(ip);
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr=inet_addr(ip);
     serverAddr.sin_port = htons(port); // htons() converts port number to network byte order from host byte order
@@ -52,13 +55,15 @@ int main()
     cout << "[*] Waiting for confirmation from server." << endl;
     recv(client, buffer, BUFSIZE, 0);
     cout << "[:)] Connection confirmed!" << endl;
-   // cout << "[*] Enter your username" << endl;
-   // cout << "Username: " << endl;
-    //gets(username);
-    cout <<"[!] Enter # (i.e., Shift + 3), to end this connection" << endl;
+    cout << "[*] Enter your username" << endl;
+    cout << "Username: " << endl;
+    gets(username);
+    cout <<"[*] Please append all your messages with a #" << endl;
+    // We need to replace ^ with enter
     // client's message goes first
+    cout<<"[*] Client's message goes first. Say hi!"<<endl;
    do {
-        cout << "Client: ";
+        cout <<username<<": ";
         do {
             cin >> buffer;
             send(client, buffer, BUFSIZE, 0);
