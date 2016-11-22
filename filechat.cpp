3e66b99c1bad8e9d8d7d465123cdfbfc0643624e
unknown <nikhilpe@gmail.com>
2016-11-22 23:33:32 +0530

    filechat: MASSIVE update
    -> filechat:main: Expand menu to include file handling options
    -> filechat:logging: Fix logging issues
    -> filechat:logging: Use proprietary extension for chat history
    -> filechat:main: define functions for file handling ops
    -> filechat:chat: Display previous chat history during connection (if exists)
    -> filechat:chat: Display timestamp during chat
    
    What works: Most of the options in the menu. Things are looking pretty good.
    What's broken: Some messages are getting duplicated when they're stored in the file. Don't know why.
    			   Random single spaces coming in console output during chat.
    			   # to end connection.
    			   Still have to press enter twice to send message.
    			   Maybe more. Didn't do much testing.

--- filechat.cpp
+++ filechat.cpp
@@ -5,8 +5,10 @@
 #include <winsock2.h>
 #include <time.h>
 
-
 using namespace std;
+
+fstream ifof;
+ofstream cr8;
 
 struct Log
 {
@@ -19,12 +21,112 @@ struct Log
     int sec;
     char message[1024];
 };
-fstream ifof;
-ofstream cr8;
+
+void dispChat(char* name, int ch=0)
+{
+    Log l1;
+    fstream fin;
+    char nof[120];
+    strcpy(nof, name);
+    strcat(nof, ".hist");
+    fin.open(nof, ios::in|ios::binary);
+    while(fin)
+    {
+        fin.read((char*)&l1, sizeof(Log));
+        cout<<"["<<l1.hour<<":"<<l1.minute<<":"<<l1.sec<<"|";
+        cout<<l1.day<<"/"<<l1.mon<<"/"<<l1.year<<"] ";
+        cout<<l1.User<<": "<<l1.message<<endl;
+    }
+    fin.close();
+    if(ch==1){
+        cout<<endl<<"Press any key to continue..."<<endl;
+        cin.sync();
+        cin.get();
+    }
+    // cout<<endl<<"END OF FILE. THANKS"<<endl;
+    // cin.get();
+}
+
+void deleteChats(char* name, int d=0, int m=0, int y=0)
+{
+    char nof[120];
+    strcpy(nof, name);
+    strcat(nof, ".hist");
+    if(d==0 && m==0 && y==0)
+    {
+        remove(nof);
+        cout<<"[*] History of chats with "<<name<<" deleted! Press any key to continue..."<<endl;
+        cin.sync();
+        cin.get();
+        return;
+    }
+    ifstream fin; ofstream fout;
+    fin.open(nof, ios::in|ios::binary);
+    fout.open("temp.hist", ios::out|ios::binary);
+    Log l1;
+    while(fin)
+    {
+        fin.read((char*)&l1, sizeof(Log));
+        if(l1.day==d && l1.mon==m && l1.year==y)
+            continue;
+        fout.write((char*)&l1, sizeof(Log));
+    }
+    fin.close();
+    fout.close();
+    remove(nof);
+    rename("temp.hist", nof);
+    cout<<"[*] Removed all messages conveyed on the date entered! Press any key to continue..."<<endl;
+    cin.sync();
+    cin.get();
+}
+
+void renameChats(char* nuname, char* ouname)
+{
+    char nof[120], newfile[120];
+    strcpy(nof, ouname);
+    strcat(nof, ".hist");
+    strcpy(newfile, nuname);
+    strcat(newfile, ".hist");
+    ifstream fin;
+    ofstream fout;
+    fin.open(nof, ios::in|ios::binary);
+    fout.open(newfile, ios::out|ios::binary);
+    Log l1;
+    while(fin)
+    {
+        fin.read((char*)&l1, sizeof(Log));
+        if(strcmpi(ouname, l1.User)==0)
+            strcpy(l1.User, nuname);
+        fout.write((char*)&l1, sizeof(Log));
+    }
+    fin.close();
+    fout.close();
+    remove(nof);
+    cout<<"[*] Username changed!..."<<endl;
+    cin.sync();
+    cin.get();
+}
 
 /* BEGIN SERVER CLASS DEFINITION
  *******************************************
  */
+
+void timeStamp()
+{
+    int h, m, s, dd, mm, yyyy;
+    time_t rawtime;
+    struct tm * timeinfo;
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+    yyyy = timeinfo->tm_year + 1900;
+    mm = timeinfo->tm_mon + 1;
+    dd = timeinfo->tm_mday;
+    h = timeinfo->tm_hour;
+    m = timeinfo->tm_min;
+    s = timeinfo->tm_sec;
+    cout<<"["<<h<<":"<<m<<":"<<s<<"|";
+    cout<<dd<<"/"<<mm<<"/"<<yyyy<<"] ";
+}
 
  class Server{
 private:
@@ -49,12 +151,20 @@ public:
     void RecvUsername();
 
     SOCKET client, server;
-    int port = 1500;
-    int BUFSIZE =  1024;
-    bool isExit = false;
+    int port;
+    int BUFSIZE;
+    int isExit;
     char buffer[1024];
+    Server();
 
 };
+
+Server::Server()
+{
+    port = 1500;
+    BUFSIZE = 1024;
+    isExit = 0;
+}
 
 void Server::InitWinsock(){
     cout << "[*] Initialising Winsock." << endl;
@@ -69,19 +179,19 @@ void Server::InitWinsock(){
 
 void Server::InitLog(char* name)
 {
+    dispChat(extUsername);
     char filename[120];
     strcpy(filename, name);
-    strcat(filename, ".bin");
-    cr8.open(filename, ios::out);
+    strcat(filename, ".hist");
+    cr8.open(filename, ios::out|ios::app);
     cr8.close();
-    ifof.open(filename, ios::in|ios::out|ios::binary|ios::ate);
-    cout << "[:)] Log initialized" <<endl;
+    ifof.open(filename, ios::in|ios::out|ios::binary|ios::app);
 }
 
 void Server::writeLog(char* mesg, char* uname)
 {
     Log l1;
-    strcpy(l1.User, username);
+    strcpy(l1.User, uname);
     time_t rawtime;
     struct tm * timeinfo;
     time ( &rawtime );
@@ -93,14 +203,7 @@ void Server::writeLog(char* mesg, char* uname)
     l1.minute = timeinfo->tm_min;
     l1.sec = timeinfo->tm_sec;
     strcpy(l1.message, mesg);
-    int pointer = ifof.tellp();
     ifof.write((char*)&l1, sizeof(Log));
-    cout << "[:)] Message written to log" <<endl;
-    ifof.seekg(pointer);
-    Log l2;
-    ifof.read((char*)&l2, sizeof(Log));
-    cout<<"READ: "<<l2.message<<endl;
-
 }
 
 void Server::CloseLog()
@@ -127,7 +230,7 @@ void Server::killSock(){
 
 void Server::SendUsername()
 {
-    cout << "Enter the username you wish to use..." <<endl;
+    cout << "[*] Enter the username you wish to use..." <<endl;
     cin>>username;
     char toSend[120];
     strcpy(toSend, username);
@@ -177,12 +280,13 @@ void Server::sendRecvMsg()
         RecvUsername();
         SendUsername();
         cout << "[:)] Username received! " <<endl;
-        InitLog(extUsername);
         cout << "[*] Enter # (i.e. Shift + 3), to end the connection." << endl;
-        cout<<"Client's message goes first. Wait for a message..."<<endl<<endl;
+        cout<<"[*] Client's message goes first. Wait for a message..."<<endl<<endl;
         char msg[1024];
-       do {
+        InitLog(extUsername);
+        do {
             fflush(stdin);
+            timeStamp();
             cout << extUsername <<": ";
             msg[0]='\0';
              do {
@@ -195,8 +299,9 @@ void Server::sendRecvMsg()
                      isExit = true;
                  }
              } while (*buffer != 10 && *buffer != 13);
-             //writeLog(msg, extUsername);
+             writeLog(msg, extUsername);
              msg[0] = '\0';
+             timeStamp();
              cout  << "You: ";
             fflush(stdin);
             do {
@@ -210,7 +315,7 @@ void Server::sendRecvMsg()
                      isExit = true;
                  }
              } while (*buffer != 10 && *buffer != 13);
-             // writeLog(msg, username);
+              writeLog(msg, "You");
          } while (!isExit);
     killSock();
     }while(!isExit);
@@ -227,11 +332,11 @@ class Client{
 private:
     fstream ifof;
     SOCKET client;
-    int port = 1500;
+    int port;
     char username[120];
     char extUsername[120];
-    int BUFSIZE = 1024;
-    bool isExit = false;
+    int BUFSIZE;
+    int isExit;
     char buffer[1024];
     char ip[12];
     struct sockaddr_in serverAddr;
@@ -248,14 +353,24 @@ public:
 	void writeLog(char*, char*);
 	int Connekt();
 	void sendRecvMsg();
+	Client();
 
 };
+Client::Client()
+{
+    port  = 1500;
+    BUFSIZE = 1024;
+    isExit = 0;
+}
 void Client::InitLog(char* name)
 {
+    dispChat(extUsername);
     char filename[120];
     strcpy(filename, name);
-    strcat(filename, ".DAT");
-    ifof.open(filename, ios::in|ios::out|ios::binary|ios::ate);
+    strcat(filename, ".hist");
+    cr8.open(filename, ios::out|ios::app);
+    cr8.close();
+    ifof.open(filename, ios::in|ios::out|ios::binary|ios::app);
 }
 
 void Client::CloseLog()
@@ -270,12 +385,11 @@ int Client::InitWinsock()
         cout << "[!] Failed. Error Code:" << WSAGetLastError();
         return 1;
     }
-    cout << "[:)] Initialised!" << endl;
 }
 
 void Client::SendUsername()
 {
-    cout << "Enter the username you wish to use..." <<endl;
+    cout << "[*] Enter the username you wish to use..." <<endl;
     cin>>username;
     char toSend[120];
     strcpy(toSend, username);
@@ -316,7 +430,7 @@ int Client::Connekt()
     // connecting to the socket happens here
     if(connect(client, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
     {
-        cout<<"connect function failed with error: \n"<<WSAGetLastError();
+        cout<<"[!] connect function failed with error: \n"<<WSAGetLastError();
         cout << "[!] Failed to connect to port " << port << " on " << ip << endl;
         return 1;
     }
@@ -325,7 +439,7 @@ int Client::Connekt()
 void Client::writeLog(char* mesg, char* uname)
 {
     Log l1;
-    strcpy(l1.User, username);
+    strcpy(l1.User, uname);
     time_t rawtime;
     struct tm * timeinfo;
     time ( &rawtime );
@@ -349,32 +463,40 @@ void Client::sendRecvMsg()
     cout << "[*] Waiting for username exchange...please hold on." <<endl;
     RecvUsername();
     cout << "[:)] Username received! " <<endl;
-    InitLog(extUsername);
-    cout <<"[*] Please append all your messages with a *" << endl;
     cout<<"[*] Client's message goes first. Say hi!"<<endl<<endl;
+    InitLog(extUsername);
+    char msg[1024];
     do {
+        timeStamp();
         cout <<"You: ";
         fflush(stdin);
+        msg[0]='\0';
         do {
             fgets(buffer, 1024, stdin);
+            strcat(msg, buffer);
+            strcat(msg, " ");
             send(client, buffer, BUFSIZE, 0);
-            writeLog(username, buffer);
             if (*buffer == '#') {
                 send(client, buffer, BUFSIZE, 0);
                 *buffer = '*';
                 isExit = true;
             }
         } while (*buffer != 10 && *buffer != 13);
+        writeLog(msg, "You");
+        timeStamp();
         cout << extUsername << ": ";
+        msg[0]='\0';
         do {
             recv(client, buffer, BUFSIZE, 0);
             cout << buffer << " ";
-            writeLog(username, buffer);
+            strcat(msg, buffer);
+            strcat(msg, " ");
             if (*buffer == '#') {
                 *buffer = '*';
                 isExit = true;
             }
         } while (*buffer != 10 && *buffer != 13);
+        writeLog(msg, extUsername);
     } while (!isExit);
 
     KillSock();
@@ -395,6 +517,7 @@ void Client::KillSock()
 int main()
 {
     int ch, flag = 0;
+    while(1){
     do{
        if(flag){
            system("cls");
@@ -403,12 +526,19 @@ int main()
        cout<<"-------------------------FILECHAT - C++ Chat Using Sockets -------------------------"<<endl;
        cout<<"[1] Host a server"<<endl;
        cout<<"[2] Join a server"<<endl;
+       cout<<"[3] Display history of a particular chat"<<endl;
+       cout<<"[4] Delete messages selectively"<<endl;
+       cout<<"[5] Delete all messages from a particular user"<<endl;
+       cout<<"[6] Change username of existing chat history"<<endl;
+       cout<<"[7] Exit"<<endl;
        cout<<"[*] Please enter your choice"<<endl;
        cin>>ch;
-       if(ch!= 1 && ch!=2)
+       if(ch<1 || ch>7)
            flag = 1;
-    }while(ch<1 || ch>2);
+    }while(ch<1 || ch>7);
     Server s; Client clnt;
+    char nop[120], nonp[120];
+    int d, m, y;
     switch(ch)
     {
         case 1:
@@ -423,6 +553,33 @@ int main()
                 clnt.Connekt();
                 clnt.sendRecvMsg();
         break;
+        case 3: cout<<"[*] Enter name of the person whose chats you want to view..."<<endl;
+                cin>>nop;
+                dispChat(nop, 1);
+        break;
+        case 4: cout<<"[*] Enter name of the person whose chats you want to delete..."<<endl;
+                cin>>nop;
+                cout<<"[*] Enter the date of the messages you want to delete in the format DD MM YYYY"<<endl;
+                cin>>d>>m>>y;
+                deleteChats(nop, d, m, y);
+        break;
+        case 5: cout<<"[*] Enter the name of the person whose chats you want to delete..."<<endl;
+                cin>>nop;
+                deleteChats(nop);
+        break;
+        case 6: cout<<"[*] Enter the old username of the person"<<endl;
+                cin>>nop;
+                cout<<"[*] Enter the new username"<<endl;
+                cin>>nonp;
+                renameChats(nonp, nop);
+        break;
+        case 7: cout<<"[:)] Thanks for using! Press any key to exit..."<<endl;
+                cin.sync();
+                cin.get();
+                exit(0);
+        break;
+    }
+        system("cls");
     }
 }
 
